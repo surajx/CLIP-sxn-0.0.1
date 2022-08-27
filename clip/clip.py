33cd97790c8e9fa0b9a6b92b8c15330aa5251ad3
Suraj Narayanan Sasikumar <surajx@gmail.com>
2022-08-27 11:07:44 +1000

    made model downlod non-private Need to download the model in Dockefile to make the model part of the containers.

--- clip/clip.py
+++ clip/clip.py
@@ -40,7 +40,7 @@ _MODELS = {
 }
 
 
-def _download(url: str, root: str):
+def download(url: str, root: str):
     os.makedirs(root, exist_ok=True)
     filename = os.path.basename(url)
 
@@ -117,7 +117,7 @@ def load(name: str, device: Union[str, torch.device] = "cuda" if torch.cuda.is_a
         A torchvision transform that converts a PIL image into a tensor that the returned model can take as its input
     """
     if name in _MODELS:
-        model_path = _download(_MODELS[name], download_root or os.path.expanduser("~/.cache/clip"))
+        model_path = download(_MODELS[name], download_root or os.path.expanduser("~/.cache/clip"))
     elif os.path.isfile(name):
         model_path = name
     else:
